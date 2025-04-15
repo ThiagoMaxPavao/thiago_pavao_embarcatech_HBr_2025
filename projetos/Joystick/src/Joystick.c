fa33e9f5f7850eb54e3bb7cee0b58389b672bee2
Thiago Pavao <thipavao44@gmail.com>
2025-04-15 09:39:46 -0300

    Melhoria na documentação do código

--- projetos/Joystick/src/Joystick.c
+++ projetos/Joystick/src/Joystick.c
@@ -16,12 +16,14 @@
 
 // --------------------------- Joystick ---------------------------
 
+// Inicializa ADC e pinos GPIO para o joystick
 void joystick_init(uint X_pin, uint Y_pin) {
     adc_init();
     adc_gpio_init(X_pin);
     adc_gpio_init(Y_pin);
 }
 
+// Retorna as coordenadas do joystick em um intervalo de -1 a 1
 void joystick_get_XY(float *x, float *y) {
     adc_select_input(0);
     uint adc_y_raw = adc_read();
@@ -39,6 +41,7 @@ void joystick_get_XY(float *x, float *y) {
 
 ssd1306_t disp;
 
+// Desenha as coordenadas do joystick na tela OLED, de -100 a 1000
 void draw_coords(float x, float y) {
     char buffer[32];
 
@@ -54,12 +57,14 @@ void draw_coords(float x, float y) {
 // --------------------------- Lógica Principal ---------------------------
 
 int main() {
+    // Inicializa os periféricos
     stdio_init_all();
     joystick_init(JOY_X_PIN, JOY_Y_PIN);
 
     disp.external_vcc = false;
     ssd1306_init(&disp, 128, 64, 0x3C, I2C_PORT, I2C_SDA, I2C_SCL);
 
+    // Lógica principal, lê coordenadas e atualiza o display a cada 10ms
     float x, y;
 
     while (true) {
