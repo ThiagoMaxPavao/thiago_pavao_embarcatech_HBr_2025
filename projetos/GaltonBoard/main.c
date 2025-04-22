8b381d42680cb0ae2bb4153dfa1051d72cd2bf9c
Thiago Pavao <thipavao44@gmail.com>
2025-04-22 09:13:37 -0300

    add ssd1306 library

--- projetos/GaltonBoard/main.c
+++ projetos/GaltonBoard/main.c
@@ -1,14 +1,29 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "ssd1306.h"
 
+// --------------------------- Pinagem ---------------------------
 
+// I2C display OLED
+#define I2C_PORT i2c1
+#define I2C_SDA 14
+#define I2C_SCL 15
 
-int main()
-{
+// --------------------------- Display OLED ---------------------------
+
+ssd1306_t disp;
+
+int main() {
     stdio_init_all();
 
+    disp.external_vcc = false;
+    ssd1306_init(&disp, 128, 64, 0x3C, I2C_PORT, I2C_SDA, I2C_SCL);
+
+    ssd1306_clear(&disp);
+    ssd1306_draw_pixel(&disp, 0,32);
+    ssd1306_show(&disp);
+
     while (true) {
-        printf("Hello, world!\n");
         sleep_ms(1000);
     }
 }
